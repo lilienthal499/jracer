e07aa79a22be2a022be55bab66c526427b9b2015
Jan Schulte-Rebbelmund <jracer@example.com>
2026-03-20 22:05:35 +0100

    Add file-based track loading system
    Implemented simple track loading from JSON files as first step toward
    backend architecture. Tracks are now loaded dynamically at game startup.
    
    Changes:
    - Integrated track loading into jracer.startup() in application.js
    - Track JSON files fetched before game initialization
    - Config now references track by number instead of inline sections
    - Modified index.html remains clean (just calls jracer.startup)
    
    Tracks included:
    - Track 1: Beginner Oval
    - Track 2: Technical Circuit (manually created)
    
    To switch tracks: Change track.number in js/config.js
    
    Future: Add backend API, track selection UI, more tracks

--- js/application.js
+++ js/application.js
@@ -39,30 +39,45 @@ jracer.Vector = class {
 jracer.startup = function (config) {
   'use strict';
 
-  const screens = [];
+  fetch(`tracks/${config.track.number}.json`)
+    .then((response) => response.json())
+    .then((trackData) => {
+      config.track.sections = trackData.sections.map((sectionName) => {
+        return jracer.Track[sectionName];
+      });
 
-  function setupPlayers(players) {
+      config.track.gridSize = trackData.gridSize;
 
-    players.forEach((player) => {
+      console.log(`Loaded track: ${trackData.name} (${trackData.description})`);
 
-      const car = new jracer.model.Car();
-      car.controls.maxSteeringAngle = player.maxSteeringAngle;
-      //TODO
-      car.position.x = jracer.model.track.startingPosition.x;
-      car.position.y = jracer.model.track.startingPosition.y;
+      startGame(config);
+    });
 
-      jracer.model.cars.push(car);
+  function startGame(cfg) {
+    const screens = [];
 
-      const carController = new jracer.controller.CarController(car);
+    function setupPlayers(players) {
 
-      jracer.frameManager.addFrameListener(carController.update);
+      players.forEach((player) => {
 
-      const keyboardController = new jracer.controller.KeyboardController(player.controls, carController);
+        const car = new jracer.model.Car();
+        car.controls.maxSteeringAngle = player.maxSteeringAngle;
+        //TODO
+        car.position.x = jracer.model.track.startingPosition.x;
+        car.position.y = jracer.model.track.startingPosition.y;
 
-      document.addEventListener('keydown', keyboardController.getKeyHandler());
-      document.addEventListener('keyup', keyboardController.getKeyHandler());
+        jracer.model.cars.push(car);
 
-      jracer.physicsEngine.addCar(car);
+        const carController = new jracer.controller.CarController(car);
+
+        jracer.frameManager.addFrameListener(carController.update);
+
+        const keyboardController = new jracer.controller.KeyboardController(player.controls, carController);
+
+        document.addEventListener('keydown', keyboardController.getKeyHandler());
+        document.addEventListener('keyup', keyboardController.getKeyHandler());
+
+        jracer.physicsEngine.addCar(car);
 
       // if (index === 1) {
       // firstCar = car;
@@ -71,63 +86,63 @@ jracer.startup = function (config) {
       // } else {
       // carViews.push(new jracer.view.MovingCar(player.view, car));
       // }
-    });
+      });
+    }
+
+    function createScreenView(players, playerIndex) {
+
+      const carViews = [];
+
+      jracer.model.cars.forEach((car, index) => {
+        if (index !== playerIndex) {
+          carViews.push(new jracer.view.MovingCar(players[index].view, car));
+        }
+      });
+      const firstCar = jracer.model.cars[playerIndex];
+      const firstCarView = new jracer.view.StaticCar(players[playerIndex].view, firstCar);
+      const headUpDisplayView = new jracer.view.HeadUpDisplay({}, firstCar);
+
+      const tireTracksView = new jracer.view.TireTracks({}, jracer.model.cars);
+      jracer.frameManager.addSubFrameListener(tireTracksView.update);
+
+
+      const trackDrawer = new jracer.Track.Drawer(tireTracksView.getCanvas(), jracer.model.track.sequenceOfComponents, jracer.model.track.gridSize);
+      carViews.push(new jracer.view.MovingCar(players[playerIndex].view, firstCar));
+      // trackView = new jracer.view.StaticTrack(jracer.config.track, carViews);
+      const trackView = new jracer.view.MovingTrack(jracer.config.track, firstCar, carViews, tireTracksView);
+      return new jracer.view.Screen(players[playerIndex].view, trackView, firstCarView, firstCar, headUpDisplayView);
+    }
+
+    jracer.physicsEngine = new jracer.PhysicsEngine(jracer.model);
+    jracer.physicsEngine.scheduleUpdates(jracer.frameManager);
+
+    const track = new jracer.Track(config.track.sections, config.track.gridSize);
+
+    jracer.model.track = track.getModel();
+
+
+    setupPlayers(config.players);
+
+    screens.push(createScreenView(config.players, 0));
+    screens.push(createScreenView(config.players, 1));
+
+    const miniMap = new jracer.view.MiniMap();
+
+    const splitScreen = new jracer.view.SplitScreen({}, screens, miniMap);
+    window.document.body.appendChild(splitScreen.getDOMElement());
+    jracer.frameManager.addSubFrameListener(splitScreen.update);
+    // screenView2 = new jracer.view.Screen(jracer.config.screen, trackView, firstCarView, headUpDisplayView);
+
+    function createAndFocusDummyInput() {
+      const newDOMElement = window.document.createElement('input');
+      newDOMElement.autofocus = true;
+      newDOMElement.className = 'dummy';
+      window.document.body.appendChild(newDOMElement);
+    }
+    createAndFocusDummyInput();
+
+    window.setTimeout(jracer.frameManager.start, 1000);
   }
-
-  function createScreenView(players, playerIndex) {
-
-    const carViews = [];
-
-    jracer.model.cars.forEach((car, index) => {
-      if (index !== playerIndex) {
-        carViews.push(new jracer.view.MovingCar(players[index].view, car));
-      }
-    });
-    const firstCar = jracer.model.cars[playerIndex];
-    const firstCarView = new jracer.view.StaticCar(players[playerIndex].view, firstCar);
-    const headUpDisplayView = new jracer.view.HeadUpDisplay({}, firstCar);
-
-    const tireTracksView = new jracer.view.TireTracks({}, jracer.model.cars);
-    jracer.frameManager.addSubFrameListener(tireTracksView.update);
-
-
-    const trackDrawer = new jracer.Track.Drawer(tireTracksView.getCanvas(), jracer.model.track.sequenceOfComponents, jracer.model.track.gridSize);
-    carViews.push(new jracer.view.MovingCar(players[playerIndex].view, firstCar));
-    // trackView = new jracer.view.StaticTrack(jracer.config.track, carViews);
-    const trackView = new jracer.view.MovingTrack(jracer.config.track, firstCar, carViews, tireTracksView);
-    return new jracer.view.Screen(players[playerIndex].view, trackView, firstCarView, firstCar, headUpDisplayView);
-  }
-
-  jracer.physicsEngine = new jracer.PhysicsEngine(jracer.model);
-  jracer.physicsEngine.scheduleUpdates(jracer.frameManager);
-
-  const track = new jracer.Track(config.track.sections, config.track.gridSize);
-
-  jracer.model.track = track.getModel();
-
-
-  setupPlayers(config.players);
-
-  screens.push(createScreenView(config.players, 0));
-  screens.push(createScreenView(config.players, 1));
-
-  const miniMap = new jracer.view.MiniMap();
-
-  const splitScreen = new jracer.view.SplitScreen({}, screens, miniMap);
-  window.document.body.appendChild(splitScreen.getDOMElement());
-  jracer.frameManager.addSubFrameListener(splitScreen.update);
-  // screenView2 = new jracer.view.Screen(jracer.config.screen, trackView, firstCarView, headUpDisplayView);
-
-  function createAndFocusDummyInput() {
-    const newDOMElement = window.document.createElement('input');
-    newDOMElement.autofocus = true;
-    newDOMElement.className = 'dummy';
-    window.document.body.appendChild(newDOMElement);
-  }
-  createAndFocusDummyInput();
-
-  window.setTimeout(jracer.frameManager.start, 1000);
-
 };
 
 /*
--- js/config.js
+++ js/config.js
@@ -5,38 +5,42 @@ jracer.config = {
     split: false
   },
   track: {
-    number: 2, // Which track to load from tracks/ directory
+    number: 1, // Which track to load from tracks/ directory
     gridSize: 400, // Will be overridden by track data
     sections: [], // Will be populated from track JSON
     grid: []
   },
-  players : [
+  players: [
     {
       name: '田阳',
-      controls: [ { key: jracer.controller.Keys.UP, code: 87 }, // w
+      controls: [
+        { key: jracer.controller.Keys.UP, code: 87 }, // w
         { key: jracer.controller.Keys.DOWN, code: 83 }, // s
         { key: jracer.controller.Keys.LEFT, code: 65 }, // a
-        { key: jracer.controller.Keys.RIGHT, code: 68 } ], // d
-      view: { color: 'DarkSeaGreen',
-        rotateAndZoom: false },
+        { key: jracer.controller.Keys.RIGHT, code: 68 }
+      ], // d
+      view: { color: 'DarkSeaGreen', rotateAndZoom: false },
       maxSteeringAngle: 0.2
     },
     {
       name: '田南',
-      controls: [ { key: jracer.controller.Keys.UP, code: 38 },
+      controls: [
+        { key: jracer.controller.Keys.UP, code: 38 },
         { key: jracer.controller.Keys.DOWN, code: 40 },
         { key: jracer.controller.Keys.LEFT, code: 37 },
-        { key: jracer.controller.Keys.RIGHT, code: 39 } ],
-      view: { color: 'Maroon',
-        rotateAndZoom: true },
+        { key: jracer.controller.Keys.RIGHT, code: 39 }
+      ],
+      view: { color: 'Maroon', rotateAndZoom: true },
       maxSteeringAngle: 0.17
     },
     {
       name: 'Dummy',
-      controls: [ { key: jracer.controller.Keys.UP, code: 73 },		// i
-        { key: jracer.controller.Keys.DOWN, code: 75 },		// k
-        { key: jracer.controller.Keys.LEFT, code: 74 },		// j
-        { key: jracer.controller.Keys.RIGHT, code: 76 } ],	// l
+      controls: [
+        { key: jracer.controller.Keys.UP, code: 73 }, // i
+        { key: jracer.controller.Keys.DOWN, code: 75 }, // k
+        { key: jracer.controller.Keys.LEFT, code: 74 }, // j
+        { key: jracer.controller.Keys.RIGHT, code: 76 }
+      ], // l
       view: { color: 'Olive' },
       maxSteeringAngle: 0.15
     }
